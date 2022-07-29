ed93f9cbf4aa6f389f581a7340675c2e8fd7ea0c
JuanZubillaga <juanzubillagap@gmail.com>
2022-07-29 16:28:53 -0300

    articulo individual por id

--- routes/publicRoutes.js
+++ routes/publicRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const publicRouter = express.Router();
-const pageController = require("../controllers/pagesController")
+const pageController = require("../controllers/pagesController");
+const articleController = require("../controllers/articleController");
 
 // showHome
 
-publicRouter.get("/", pageController.showHome)
-
+publicRouter.get("/", pageController.showHome);
+publicRouter.get("/article/:id", articleController.show);
 module.exports = publicRouter;
