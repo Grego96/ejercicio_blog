fb5a2890dc2fa9e63da05249aa11a9442df2c77f
Sebastián Guadalupe <sebastianguadalupe00@gmail.com>
2022-08-02 14:38:09 -0300

    integramos passport

--- server.js
+++ server.js
@@ -1,10 +1,47 @@
 require("dotenv").config();
 
 const express = require("express");
+const bcrypt = require("bcrypt");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
 const routes = require("./routes");
 const dbInitialSetup = require("./dbInitialSetup");
 const APP_PORT = process.env.APP_PORT || 3000;
 const app = express();
+
+app.use(session({ secret: "AlgúnTextoSuperSecreto", resave: false, saveUninitialized: false }));
+app.use(passport.session());
+passport.use(
+  new LocalStrategy(async (email, password, done) => {
+    const user = await User.findOne({ email: email }, function (err, user) {
+      if (err) {
+        return done(err);
+      }
+    });
+    if (!user) {
+      return done(null, false, { message: "Incorrect email or password." });
+    }
+    bcrypt.compare(user.password, password, function (err, result) {
+      return done(null, user);
+    });
+    return done(null, false, { message: "Incorrect email or password." });
+  }),
+);
+
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+  const user = await User.findByPk(id)
+    .then((user) => {
+      done(null, user);
+    })
+    .catch((error) => {
+      done(error, user);
+    });
+});
 
 app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: true }));
