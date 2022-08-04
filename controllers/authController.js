7a4b4583480c08559de5a63db44f9a16751b6e6f
Pablo Méndez <valentino.mendez@correo.ucu.edu.uy>
2022-08-04 11:29:38 -0300

    changes in authcontroller

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,32 +1,3 @@
-const { Article, User, Comment } = require("../models");
-const passport = require("passport");
-const bcrypt = require("bcrypt");
-
-// Display a listing of the resource.
-async function index(req, res) { }
-
-// Display the specified resource.
-async function show(req, res) { }
-
-// Show the form for creating a new resource
-async function create(req, res) { }
-
-// Store a newly created resource in storage.
-async function store(req, res) { }
-
-// Show the form for editing the specified resource.
-async function edit(req, res) { }
-
-// Update the specified resource in storage.
-async function update(req, res) { }
-
-// Remove the specified resource from storage.
-async function destroy(req, res) { }
-
-async function loginUser(req, res) {
-    // const user= User.findOne({Where: {email:email}})
-};
-
 function showLogin(req, res) {
     res.render("login");
 }
@@ -45,24 +16,7 @@ function logOutUser(req, res) {
     });
 }
 
-
-
-/*async function adminUser() {
-
-}
-*/
-// Otros handlers...
-// ...
-
 module.exports = {
-    index,
-    show,
-    create,
-    store,
-    edit,
-    update,
-    destroy,
-    loginUser,
     showLogin,
     showRegister,
     logOutUser
