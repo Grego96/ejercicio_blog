4f8bb471d6890763bcd92cfba2ce6de2cdb09618
Sebastián Guadalupe <sebastianguadalupe00@gmail.com>
2022-08-04 10:57:16 -0300

    sacar console log en isAuthenticated.js

--- middlewares/isAuthenticated.js
+++ middlewares/isAuthenticated.js
@@ -1,6 +1,5 @@
 module.exports = function (req, res, next) {
   if (!req.isAuthenticated()) {
-    console.log("ACA -> ", req);
     req.session.redirectTo = req.query.redirectTo;
     return res.redirect("/login");
   }
